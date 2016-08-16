57dfcd7cac20d38d33b48fd5bea63b2c60c99aaf
lijing <lijing01@corp.netease.com>
2016-08-16 13:38:25 +0800

    no message

---
+++ GzipUtil/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GzipUtil
+//
+//  Created by Lisa on 16/8/16.
+//  Copyright © 2016年 Lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
